836c1a12c30c1682e907b16d86e33528e185f6db
Maxim Alov <maxim@aludio.com>
2018-12-11 19:19:57 +0300

    Added Rest adapter

---
+++ data/src/main/kotlin/com/orcchg/githubuser/data/remote/GithubRestAdapter.kt
@@ -0,0 +1,15 @@
+package com.orcchg.githubuser.data.remote
+
+import com.orcchg.githubuser.domain.model.User
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GithubRestAdapter {
+
+    @GET("users")
+    fun users(): Single<List<User>>
+
+    @GET("users/{login}")
+    fun user(@Path("login") login: String)
+}
